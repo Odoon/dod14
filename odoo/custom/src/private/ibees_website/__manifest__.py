3e3c0f6e5e67db8ee67b2be76b00a5aa968d59dc
Syed Ali <go4siteg@gmail.com>
2020-12-26 20:05:10 +0000

    Seperate dev and prod docker-compose files8

---
+++ odoo/custom/src/private/ibees_website/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": "iBees Website",
+    "summary": """
+        iBees mod for website customizations""",
+    "author": "iBees",
+    "website": "http://www.ibees.biz",
+    "license": "AGPL-3",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Website/Website",
+    "version": "14.0.0.0.0",
+    # any module necessary for this one to work correctly
+    "depends": ["website", "portal"],
+    # always loaded
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/assets.xml",
+        "views/debranding.xml",
+        "views/templates.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": [
+        "demo/demo.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
