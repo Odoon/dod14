19b3a29b666e6c772b3424aeddc45f5dba4b8acf
Syed Ali <go4siteg@gmail.com>
2020-12-29 01:19:14 +0000

    add k8s depl

--- odoo/custom/src/private/ibees_website/__manifest__.py
+++ odoo/custom/src/private/ibees_website/__manifest__.py
@@ -6,7 +6,8 @@
     "website": "http://www.ibees.biz",
     "license": "AGPL-3",
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/
+    # addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Website/Website",
     "version": "14.0.0.0.0",
